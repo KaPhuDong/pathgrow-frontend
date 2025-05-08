6de9bad6859804de48f93f40310d220ca4792eeb
ChangSeven2k5 <changcoor7@gmail.com>
2025-05-09 00:27:36 +0700

    edit student profile ui about folder name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Header from './layout/student/Header/Header';
-import Content from './pages/student/StudentProfile/StudentProfile';
-import Footer from './layout/student/Footer/Footer';
+import Header from './layout/student/header/Header';
+import Content from './pages/student/student-profile/StudentProfile';
+import Footer from './layout/student/footer/Footer';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
