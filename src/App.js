45aa3a17fcae58a0e6114d8cee28a7dac846c2c5
ChangSeven2k5 <changcoor7@gmail.com>
2025-05-08 23:50:51 +0700

    build student profile ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './layout/student/Header/Header';
+import Content from './pages/student/StudentProfile/StudentProfile';
+import Footer from './layout/student/Footer/Footer';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App-PathGrow'>
+      <Header />
+      <main>
+        <Content />
+      </main>
+      <Footer />
     </div>
   );
 }
