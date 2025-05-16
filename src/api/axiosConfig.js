46228d161b5314a78083f7c5a2c112706db33374
KaPhuDong <dong.ka26@student.passerellesnumeriques.org>
2025-05-16 10:12:07 +0700

    fix logout btn

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -7,25 +7,42 @@ const instance = axios.create({
   },
 });
 
-instance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+export const setupAxiosInterceptors = (startLoading, stopLoading) => {
+  // Request Interceptor
+  instance.interceptors.request.use(
+    (config) => {
+      startLoading && startLoading();
 
-instance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      console.log('Unauthorized. Redirect to login...');
+      const token = localStorage.getItem('token');
+      if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+      }
+
+      return config;
+    },
+    (error) => {
+      stopLoading && stopLoading();
+      return Promise.reject(error);
     }
-    return Promise.reject(error);
-  }
-);
+  );
+
+  // Response Interceptor
+  instance.interceptors.response.use(
+    (response) => {
+      stopLoading && stopLoading();
+      return response;
+    },
+    (error) => {
+      stopLoading && stopLoading();
+
+      if (error.response?.status === 401) {
+        console.log('Unauthorized. Redirect to login...');
+        // Optional: window.location.href = '/login';
+      }
+
+      return Promise.reject(error);
+    }
+  );
+};
 
 export default instance;
